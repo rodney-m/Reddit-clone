07be3bb28ded51d3b3653163aaac16d81636000c
Rodney Mupanduki <rodneymupanduki@gmail.com>
2021-09-23 12:51:14 +0300

    Reddit clone

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Article } from './article/article.model';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  articles: Article[];
+  constructor(){
+    this.articles = [
+      new Article('Angular', 'https://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack', 2),
+      new Article('Abgular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link : HTMLInputElement) : boolean{
+    console.log(`Adding article title : ${title.value} and link : ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+}
